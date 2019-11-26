9fa4b872f8c39ae163c08106ed0df4cd0dc84dad
Jacky Liang <liangj97@gmail.com>
2019-11-27 00:28:09 +1100

    Fix animation error

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -10,16 +10,16 @@ export const fadeInAnimation =
   trigger('routeAnimations', [
     transition('* => *', [
       query(':enter, :leave',
-        style({ position: 'fixed', width: '100%' })),
+        style({ position: 'fixed', width: '100%' }), { optional: true }),
       group([
         query(':enter', [
           style({ opacity: 0, right: '3%' }),
           animate('1s 0.5s ease-out', style({ opacity: 1, right: '0' }))
-        ]),
+        ], { optional: true }),
         query(':leave', [
           style({ opacity: 1, left: '0' }),
           animate('0.5s ease-out', style({ opacity: 0, left: '3%' }))
-        ]),
+        ], { optional: true }),
       ])
     ])
   ]
