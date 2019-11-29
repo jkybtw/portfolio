ae6a5f9aacdbd07f44c9baf5faa617f4c26e928b
Jacky <liangj97@gmail.com>
2019-11-29 20:43:26 +1100

    Add animated background

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+declare var VANTA;
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
-
+export class MainComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    VANTA.FOG({
+      el: '#welcome-container-id',
+      highlightColor: 0xfaffff,
+      midtoneColor: 0xdbe1ff,
+      lowlightColor: 0xdbe1ff,
+      baseColor: 0xedf0ff,
+      blurFactor: 0.5,
+      speed: 3.5,
+      zoom: 3
+    });
   }
 
 }
